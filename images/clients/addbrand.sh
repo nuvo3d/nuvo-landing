06c6064b4eab2f5b7db8e282e331ff049a6e2813
Cristian Duguet <cd@insidernavigation.com>
2019-05-19 20:30:09 +0200

    interation no 2

--- images/clients/addbrand.sh
+++ images/clients/addbrand.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
+grayscale=''
 
-# gray 
-convert $1 -fuzz 50% -fill gray -opaque gray $2.png
-convert $2.png -resize 600x200 -background transparent  -gravity center -extent 600x200 $2@2x.png
-mv $2@2x.png gray
-convert $2.png -resize 300x100 -background transparent  -gravity center -extent 300x100 $2.png
-mv $2.png gray
+# print instructions
+print_usage() {
+	printf "Usage addbrand.sh [-g] <input_file.png> <output_number>"
+}
 
-#white
+while getopts ':gh' flag; do
+	case "${flag}" in 
+		g ) grayscale='true' ;;
+		h ) print_usage ;;
+	    \? ) echo "Invalid option: -$OPTARG" 1>&2 ; exit ;;
+	esac 
+done
+shift $((OPTIND -1))
 
-convert $1 -fuzz 50% -fill white -opaque gray $2.png
-convert $2.png -resize 600x200 -background transparent  -gravity center -extent 600x200 $2@2x.png
-mv $2@2x.png white
-convert $2.png -resize 300x100 -background transparent  -gravity center -extent 300x100 $2.png
-mv $2.png white
+file_in=$1; shift
+out=$1
+
+for color in color gray white; do
+	echo Exporting to $color/...
+	if [[ $color = 'color' ]]; then
+		cp $file_in $out.png
+	else 
+		if [[ "${grayscale}" ]]; then
+			convert $file_in -colorspace gray $out.png
+		else
+			convert $file_in -fuzz 50% -fill $color -opaque gray $out.png
+		fi
+	fi
+	
+	convert $out.png -resize 600x200 -background transparent  -gravity center -extent 600x200 $out@2x.png
+	mv $out@2x.png $color
+	convert $out.png -resize 300x100 -background transparent  -gravity center -extent 300x100 $out.png
+	mv $out.png $color
+done
 
 
